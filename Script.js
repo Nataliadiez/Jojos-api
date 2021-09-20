fc5021691701552a3b87c30db2228d21cdeb3e70
Nataliadiez <natalia.diez10@hotmail.com>
2021-09-20 00:07:24 -0300

    efecto sonidos

--- Script.js
+++ Script.js
@@ -3,9 +3,28 @@ const stand = document.querySelector("#stand")
 const imguser = document.querySelector("#img-user")
 const imgstand = document.querySelector("#img-stand")
 const audio = document.querySelector("#audio-mundo")
+const div_card = document.querySelector(".card")
+const musica_mundo = document.querySelector(".musica-mundo")
+const animacion_img_inicio = document.querySelector(".animate__animated")
 
 
+const efectos = () => {
+    animacion_img_inicio.classList.add("animate__flip")
+    let efecto_sonido = document.querySelector("#efecto-sonido")
+    efecto_sonido.src = "./audio/sawardo audio.mp3"
+}
+    
 
+
+musica_mundo.addEventListener("change", (event) => {
+    if(musica_mundo.value === "1") {
+        audio.src = "./audio/Stardust crusaders.mp3"
+    } else if (musica_mundo.value === "2") {
+        audio.src = "./audio/Diamond Is Unbreakable.mp3"
+    } else if (musica_mundo.value === "3") {
+        audio.src = "./audio/Golden wind.mp3"
+    }
+})
 document.querySelector("#boton-click").addEventListener("click", function(){
     reunirdatos()
     
@@ -25,29 +44,32 @@ const reunirdatos = () => {
                 let mundo;
                 if(eleccionparte === "1") {
                     mundo = jojos.StardustCrusaders
-                    audio.src = "./audio/Stardust crusaders.mp3"
                     numuser = Math.round(Math.random()*32);
+                    div_card.classList.remove("oculto")
                     console.log(numuser)  
                 } else if (eleccionparte === "2") {
                     mundo = jojos.DiamondIsUnbreakable
-                    audio.src = "./audio/Diamond Is Unbreakable.mp3"
                     numuser = Math.round(Math.random()*28);
+                    div_card.classList.remove("oculto")
                     console.log(numuser)  
                 } else {
                     mundo = jojos.GoldenWind
-                    audio.src = "./audio/Golden wind.mp3"
                     numuser = Math.round(Math.random()*28);
+                    div_card.classList.remove("oculto")
                     console.log(numuser)  
                 }
                 
-                for (let i = 0; i < mundo.length; i++) {
+                    mundo.forEach(x => {
+                        console.log(mundo[parseInt(numuser)].user)
+                        user.innerHTML = mundo[parseInt(numuser)].user
+                        stand.innerHTML = mundo[parseInt(numuser)].Stand
+                        imguser.src = mundo[parseInt(numuser)].user_image
+                        imgstand.src = mundo[parseInt(numuser)].stand_image
+                        
+                    });
                     
-                    console.log(mundo[parseInt(numuser)].user)
-                    user.innerHTML = mundo[parseInt(numuser)].user
-                    stand.innerHTML = mundo[parseInt(numuser)].Stand
-                    imguser.src = mundo[parseInt(numuser)].user_image
-                    imgstand.src = mundo[parseInt(numuser)].stand_image
-                }
+                    
+                
             }
 
         }
